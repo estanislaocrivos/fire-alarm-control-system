8997e42fc559b3ce0a0019ce42f013f58920a991
Estanislao Crivos <estanislaocrivos@gmail.com>
2026-08-09 23:04:24 -0300

    feat: add build for atmega2560

---
+++ scripts/build_atmega2560.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cmake -S . -B build \
+    -DCMAKE_TOOLCHAIN_FILE=external/c-libraries/cmake/toolchains/avr.cmake \
+    -DBOARD=arduino_mega_2560 \
+    -DMCU_FLAGS="-mmcu=atmega2560" \
+    -DF_CPU=16000000UL
+
+cmake --build build
